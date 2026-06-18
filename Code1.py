f3c8338cc5461a4eff9ed69564fadcf48ee27e12
evelynjiffy <evelynjiffy@gmail.com>
2026-06-18 10:53:42 +0800

    Add files via upload

---
+++ Code1.py
@@ -0,0 +1,14 @@
+print('Welcome to the world of programming')
+print('Hello World')
+print(' I like to paint')
+print(5)
+print(6+3)
+print(8//2)
+#print(8+2)
+"""
+print(5-3)
+
+print(9//3)
+"""
+print(" The sum of 3 and 5 is: ", 3+5)
+print(" My lucky number is 7")
---
+++ code2.py
@@ -0,0 +1,13 @@
+print(" Hello World!\n", "Hello python\n", 7 )
+
+print(1,"\n",2,"\n", 3)
+
+print("Hello", 5, end= " ")
+print(" Hi", " bye" )
+
+
+x= input ("What is your age? ")
+print(x, ",is my age")
+
+import keyword
+print(keyword.kwlist)

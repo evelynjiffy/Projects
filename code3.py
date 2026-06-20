96d919e046db4181f1026669c9e7f0cac7eab98d
evelynjiffy <evelynjiffy@gmail.com>
2026-06-20 11:08:11 +0800

    Add files via upload

---
+++ code3.py
@@ -0,0 +1,25 @@
+a=5
+print("type of a:", type(a))
+b= True
+print(" type of b:", type(b), "and value of b is:", b)
+c= "codingal" 
+print(c, " is of type:", type(c))
+d= 2.5
+print(type(d))
+a= input("What is your name? ")
+print(a)
+print(a[0:3])
+print(a[ 4])
+
+b= input("What is your last name? ")
+print("Fullname:", a+ " "+b)
+
+a=5
+b=1.2
+c=True
+d="python"
+
+print("a when converted to float using float() is:",float(a) )
+print("b when converted to integer using int()is:", int(b))
+print("c when converted to string using str() is:", str(c))
+print("d when convertes to bool using bool() is:", bool(d))

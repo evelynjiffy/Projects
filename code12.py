e2fdec65f5d2b82c5946ddaa2f54495c4227fd43
evelynjiffy <evelynjiffy@gmail.com>
2026-06-28 10:55:29 +0800

    Add files via upload

---
+++ code12.py
@@ -0,0 +1,41 @@
+"""#1) Store the given values:
+
+# `mean1` (wrong mean), `wrong_number`, `correct_number`, and
+#  `total_number`.
+
+# 2) Calculate the total sum using the wrong mean:
+
+# - Multiply `mean1` by `total_number`
+
+# - Store it in `sum`
+
+# - Print the sum.
+
+# 3) Fix the sum to get the correct total:
+
+# - Remove the wrong number (subtract `wrong_number`)
+
+# - Add the correct number (add `correct_number`)
+
+# - Store the corrected total in `num2`
+
+# - Print the corrected sum.
+
+# 4) Find the correct mean:
+
+# - Divide `num2` by `total_number`
+
+# - Store it in `mean2`
+
+# - Print `mean2`.
+
+The mean of 40 numbers is 38. Later on, I detected that misread the
+ number 56 as 36. Find the correct mean of given numbers.
+
+"""
+total_number=40
+mean1=38
+sum1= total_number*mean1
+sum2= sum1+20
+mean2=sum2/total_number
+print(mean2)
